712f887cbb75ccee7a849bc247e922e1b111232e
loressl <loryssl@hotmail.com>
2019-10-25 22:37:00 -0300

    Add page resetpassword.

--- flutter_avengers_life2/lib/pages/reset.password.page.dart
+++ flutter_avengers_life2/lib/pages/reset.password.page.dart
@@ -5,16 +5,100 @@ class ResetPasswordPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        automaticallyImplyLeading: true,
-        leading: IconButton(//uma das posições, canto superior esquerdo
+        backgroundColor: Colors.grey[600],
+        leading: IconButton(
           icon: Icon(Icons.arrow_back),
           color: Colors.white,
-          onPressed: ()=>Navigator.pop(context,false),
+          onPressed: ()=>Navigator.pop(context),
         ),
       ),
       body: Container(
-
+        padding: EdgeInsets.only(top: 60, left: 40, right: 40),
+        color: Colors.black,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Image.asset("assets/forgot-password-icon-6.jpg.png"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Did you forget your password?",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Please enter the email address associated with your account and we will send you a link to your account with instructions for resetting your password.",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: "E-mail",
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+              style: TextStyle(fontSize: 15),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 60,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.3,1],
+                  colors: [
+                    Color(0xFF757575),
+                    Color(0xFF000000),
+                  ],
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                ),
+              ),
+              child: SizedBox.expand(
+                child: FlatButton(
+                  child: Text(
+                    "Send",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  onPressed: (){},
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
